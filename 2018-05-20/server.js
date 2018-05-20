cdedbddad40bff5f3a3f8eabcc8afa1bd564a22e
tanzhuguo <tanzhuguo@andyun.cn>
2018-05-20 12:41:52 +0800

    add

---
+++ 2018-05-20/server.js
@@ -0,0 +1,17 @@
+
+
+
+var http = require("http");
+
+http.createServer(function(request,response){
+	// 发送http头部
+	// http 状态值 ： 200 : ok
+	// 内容类型 :text/plain
+	response.writeHead(200 , {'Content-Type':'text/plain'});
+
+	// 发送响应数据 "hello world"
+	response.end('Hello World\n');
+}).listen(8888);
+
+//终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');
